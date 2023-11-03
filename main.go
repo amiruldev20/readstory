12399c243fc7db4cb019b9993a0b91fac7ffedf1
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-11-03 15:13:45 +0000

    process

---
+++ main.go
@@ -0,0 +1,169 @@
+/*
+###################################
+# Name: Mywa BOT                  #
+# Version: 1.0.1                  #
+# Developer: Amirul Dev           #
+# Library: waSocket               #
+# Contact: 085157489446           #
+###################################
+# Thanks to: 
+# Vnia
+*/
+package main
+
+import (
+    "context"
+    "encoding/base64"
+    "fmt"
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+    "strings"
+
+    "mywabot/system/message"
+
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/mdp/qrterminal"
+    "github.com/probandula/figlet4go"
+    "github.com/joho/godotenv"
+
+    "github.com/amiruldev20/waSocket"
+    waProto "github.com/amiruldev20/waSocket/binary/proto"
+    "github.com/amiruldev20/waSocket/store"
+    "github.com/amiruldev20/waSocket/store/sqlstore"
+    "github.com/amiruldev20/waSocket/types/events"
+    "github.com/amiruldev20/waSocket/types"
+    waLog "github.com/amiruldev20/waSocket/util/log"
+
+    "google.golang.org/protobuf/proto"
+)
+
+func main() {
+    err := godotenv.Load()
+    if err != nil {
+        panic("Error load file .env")
+    }
+
+    dbLog := waLog.Stdout("Database", "ERROR", true)
+    dxz,
+    err := base64.StdEncoding.DecodeString("TVlXQSBCT1Q=")
+    if err != nil {
+        panic("malformed input")
+        log.Println(dxz)
+    }
+    container,
+    err := sqlstore.New("sqlite3", "file:mywabot.db?_foreign_keys=on", dbLog)
+    if err != nil {
+        panic(err)
+    }
+
+    deviceStore,
+    err := container.GetFirstDevice()
+    if err != nil {
+        panic(err)
+    }
+    clientLog := waLog.Stdout("Client", "ERROR", true)
+
+    /* setting env */
+        typeLogin := os.Getenv("TYPE_LOGIN")
+    numberBot := os.Getenv("BOT_NUMBER")
+
+    /* client */
+        sock := waSocket.NewClient(deviceStore, clientLog)
+    eventHandler := registerHandler(sock)
+    sock.AddEventHandler(eventHandler)
+
+    if sock.Store.ID == nil {
+        if typeLogin == "code" {
+            fmt.Println("You login with pairing code")
+            fmt.Println("Bot Number: " + numberBot)
+
+            err = sock.Connect()
+            if err != nil {
+                panic(err)
+            }
+
+            /* don't edit */
+            code, err := sock.PairPhone(numberBot, true, waSocket.PairClientChrome, "Chrome (Linux)")
+
+            if err != nil {
+                fmt.Println(err)
+                return
+            }
+            log.Println("Your Code: " + code)
+
+        } else {
+            qrChan,
+            _ := sock.GetQRChannel(context.Background())
+
+                err = sock.Connect()
+            if err != nil {
+                panic(err)
+            }
+
+            for evt := range qrChan {
+                if evt.Event == "code" {
+                    qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+                    dxz, err := base64.StdEncoding.DecodeString("TWFkZSBieSBBbWlydWwgRGV2LiBmb2xsb3cgSUcgQGFtaXJ1bC5kZXY=")
+
+                    if err != nil {
+                        panic("malformed input")
+                    }
+                    log.Println(string(dxz))
+
+                    log.Println("Please scan this QR...")
+                } else {
+                    log.Println("Login successfully!!")
+                }
+            }
+        }
+    } else {
+
+        /* Already logged in, just connect */
+        err = sock.Connect()
+        log.Println("Login Sucessfully!!")
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    c := make(chan os.Signal)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM) <
+    -c
+
+    sock.Disconnect()
+}
+
+func init() {
+    ascii := figlet4go.NewAsciiRender()
+    dxz,
+    err := base64.StdEncoding.DecodeString("TXl3YSBCT1QgYnkgd2FTb2NrZXQ=")
+    if err != nil {
+        panic("malformed input")
+    }
+    renderStr,
+    _ := ascii.Render(string(dxz))
+    store.DeviceProps.PlatformType = waProto.DeviceProps_FIREFOX.Enum()
+    store.DeviceProps.Os = proto.String(string(dxz))
+    fmt.Print(renderStr)
+}
+
+func registerHandler(sock * waSocket.Client) func(evt interface {}) {
+    return func(evt interface {}) {
+        switch v := evt.(type) {
+            case *events.Message:
+                if strings.HasPrefix(v.Info.ID, "BAE5") {
+                    return
+                }
+                if v.Info.Chat.String() == "status@broadcast" {
+                    sock.MarkRead([] types.MessageID {
+                        v.Info.ID
+                    }, v.Info.Timestamp, v.Info.Chat, v.Info.Sender)
+                    fmt.Println("Berhasil melihat status", v.Info.PushName)
+                }
+                go message.Msg(sock, v)
+                break
+        }
+    }
+}
---
+++ system/lib/waSocket.go
@@ -0,0 +1,239 @@
+/*
+###################################
+# Name: Mywa BOT                  #
+# Version: 1.0.1                  #
+# Developer: Amirul Dev           #
+# Library: waSocket               #
+# Contact: 085157489446           #
+###################################
+# Thanks to: 
+# Vnia
+*/
+package lib
+
+import (
+    "context"
+    "fmt"
+    "strings"
+    "net/http"
+    "os"
+    "os/exec"
+    "bytes"
+    "image/jpeg"
+
+    "github.com/amiruldev20/waSocket"
+    waProto "github.com/amiruldev20/waSocket/binary/proto"
+    "github.com/amiruldev20/waSocket/types"
+    "github.com/amiruldev20/waSocket/types/events"
+
+    "google.golang.org/protobuf/proto"
+)
+
+type renz struct {
+    sock * waSocket.Client
+    Msg * events.Message
+}
+
+func NewSimp(Cli * waSocket.Client, m * events.Message) * renz {
+    return &renz {
+        sock: Cli,
+        Msg: m,
+    }
+}
+
+/* parse jid */
+func(m * renz) parseJID(arg string)(types.JID, bool) {
+    if arg[0] == '+' {
+        arg = arg[1: ]
+    }
+    if !strings.ContainsRune(arg, '@') {
+        return types.NewJID(arg, types.DefaultUserServer), true
+    } else {
+        recipient,
+        err := types.ParseJID(arg)
+        if err != nil {
+            fmt.Println("Invalid JID %s: %v", arg, err)
+            return recipient, false
+        } else if recipient.User == "" {
+            fmt.Println("Invalid JID %s: no server specified", arg)
+            return recipient, false
+        }
+        return recipient,
+        true
+    }
+}
+
+/* send react */
+func(m * renz) React(react string) {
+    _,
+    err := m.sock.SendMessage(context.Background(), m.Msg.Info.Chat, m.sock.BuildReaction(m.Msg.Info.Chat, m.Msg.Info.Sender, m.Msg.Info.ID, react))
+    if err != nil {
+        return
+    }
+}
+
+/* send message */
+func(m * renz) SendMsg(jid types.JID, teks string) {
+    _,
+    err := m.sock.SendMessage(context.Background(), jid, & waProto.Message {
+        Conversation: proto.String(teks)
+    })
+    if err != nil {
+        return
+    }
+}
+
+/* send reply */
+func(m * renz) Reply(teks string) {
+    _,
+    err := m.sock.SendMessage(context.Background(), m.Msg.Info.Chat, & waProto.Message {
+        ExtendedTextMessage: & waProto.ExtendedTextMessage {
+            Text: proto.String(teks),
+            ContextInfo: & waProto.ContextInfo {
+                Expiration: proto.Uint32(86400),
+                StanzaId: & m.Msg.Info.ID,
+                Participant: proto.String(m.Msg.Info.Sender.String()),
+                QuotedMessage: m.Msg.Message,
+            },
+        },
+    })
+    if err != nil {
+        return
+    }
+}
+
+/* send adReply */
+func(m * renz) ReplyAd(teks string) {
+    var isImage = waProto.ContextInfo_ExternalAdReplyInfo_IMAGE
+    _, err := m.sock.SendMessage(context.Background(), m.Msg.Info.Chat, & waProto.Message {
+        ExtendedTextMessage: & waProto.ExtendedTextMessage {
+            Text: proto.String(teks),
+            ContextInfo: & waProto.ContextInfo {
+                ExternalAdReply: & waProto.ContextInfo_ExternalAdReplyInfo {
+                    Title: proto.String("MywaBOT 2023"),
+                    Body: proto.String("Made with waSocket by Amirul Dev"),
+                    MediaType: & isImage,
+                    ThumbnailUrl: proto.String("https://telegra.ph/file/eb7261ee8de82f8f48142.jpg"),
+                    MediaUrl: proto.String("https://wa.me/stickerpack/amirul.dev"),
+                    SourceUrl: proto.String("https://chat.whatsapp.com/ByQt0u0bz4NJfNPEUfDHps"),
+                    ShowAdAttribution: proto.Bool(true),
+                    RenderLargerThumbnail: proto.Bool(true),
+                },
+                Expiration: proto.Uint32(86400),
+                StanzaId: & m.Msg.Info.ID,
+                Participant: proto.String(m.Msg.Info.Sender.String()),
+                QuotedMessage: m.Msg.Message,
+            },
+        },
+    })
+    if err != nil {
+        return
+    }
+}
+
+/* send contact */
+func(m * renz) SendContact(jid types.JID, number string, nama string) {
+    _,
+    err := m.sock.SendMessage(context.Background(), jid, & waProto.Message {
+        ContactMessage: & waProto.ContactMessage {
+            DisplayName: proto.String(nama),
+            Vcard: proto.String(fmt.Sprintf("BEGIN:VCARD\nVERSION:3.0\nN:%s;;;\nFN:%s\nitem1.TEL;waid=%s:+%s\nitem1.X-ABLabel:Mobile\nEND:VCARD", nama, nama, number, number)),
+            ContextInfo: & waProto.ContextInfo {
+                StanzaId: & m.Msg.Info.ID,
+                Participant: proto.String(m.Msg.Info.Sender.String()),
+                QuotedMessage: m.Msg.Message,
+            },
+        },
+    })
+    if err != nil {
+        return
+    }
+}
+
+/* create channel */
+func(m * renz) createChannel(params[] string) {
+    _,
+    err := m.sock.CreateNewsletter(waSocket.CreateNewsletterParams {
+        Name: strings.Join(params, " "),
+    })
+    if err != nil {
+        return
+    }
+}
+
+/* fetch group admin */
+func(m * renz) FetchGroupAdmin(Jid types.JID)([] string, error) {
+    var Admin[] string
+    resp, err := m.sock.GetGroupInfo(Jid)
+    if err != nil {
+        return Admin, err
+    } else {
+        for _, group := range resp.Participants {
+            if group.IsAdmin || group.IsSuperAdmin {
+                Admin = append(Admin, group.JID.String())
+            }
+        }
+    }
+    return Admin, nil
+}
+
+/* get group admin */
+func(m * renz) GetGroupAdmin(jid types.JID, sender string) bool {
+        if !m.Msg.Info.IsGroup {
+            return false
+        }
+        admin, err := m.FetchGroupAdmin(jid)
+        if err != nil {
+            return false
+        }
+        for _, v := range admin {
+            if v == sender {
+                return true
+            }
+        }
+        return false
+    }
+
+/* get link group */
+func(m * renz) LinkGc(Jid types.JID, reset bool) string {
+    link,
+    err := m.sock.GetGroupInviteLink(Jid, reset)
+
+    if err != nil {
+        panic(err)
+    }
+    return link
+}
+
+
+func(m * renz) GetCMD() string {
+    extended := m.Msg.Message.GetExtendedTextMessage().GetText()
+    text := m.Msg.Message.GetConversation()
+    imageMatch := m.Msg.Message.GetImageMessage().GetCaption()
+    videoMatch := m.Msg.Message.GetVideoMessage().GetCaption()
+        //pollVote := m.Msg.Message.GetPollUpdateMessage().GetVote()
+    tempBtnId := m.Msg.Message.GetTemplateButtonReplyMessage().GetSelectedId()
+    btnId := m.Msg.Message.GetButtonsResponseMessage().GetSelectedButtonId()
+    listId := m.Msg.Message.GetListResponseMessage().GetSingleSelectReply().GetSelectedRowId()
+    var command string
+    if text != "" {
+        command = text
+    } else if imageMatch != "" {
+        command = imageMatch
+    } else if videoMatch != "" {
+        command = videoMatch
+    } else if extended != "" {
+        command = extended
+            /*
+            } else if pollVote != "" {
+            command = pollVote
+            */
+    } else if tempBtnId != "" {
+        command = tempBtnId
+    } else if btnId != "" {
+        command = btnId
+    } else if listId != "" {
+        command = listId
+    }
+    return command
+}
