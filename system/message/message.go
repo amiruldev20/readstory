dff1788ee719894f2be0a70bd16faac4f9a650bc
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-11-03 15:18:51 +0000

    add message

---
+++ system/message/message.go
@@ -0,0 +1,90 @@
+/*
+###################################
+# Name: Mywa BOT                  #
+# Version: 1.0.1                  #
+# Developer: Amirul Dev           #
+# Library: waSocket               #
+# Contact: 085157489446           #
+###################################
+# Thanks to: 
+# Vnia
+*/
+package message
+
+import (
+    "context"
+    "fmt"
+    "os"
+    "time"
+    "runtime"
+    "os/exec"
+    "strings"
+    "net/http"
+    "net/url"
+    "io/ioutil"
+    "encoding/json"
+
+    "github.com/joho/godotenv"
+    otto "github.com/robertkrimen/otto"
+
+    "mywabot/system/helper"
+    "mywabot/system/lib"
+    "mywabot/system/function"
+
+    // "google.golang.org/protobuf/proto"
+
+    "github.com/amiruldev20/waSocket"
+    //waProto "github.com/amiruldev20/waSocket/binary/proto"
+    "github.com/amiruldev20/waSocket/types/events"
+)
+
+var (
+    prefix = "."
+    self = false owner = "6285157489446"
+)
+
+func Msg(sock * waSocket.Client, msg * events.Message) {
+
+    err := godotenv.Load()
+    if err != nil {
+        panic("Error load file .env")
+    }
+    botNumber := os.Getenv("BOT_NUMBER")
+
+    /* my function */
+    m := lib.NewSimp(sock, msg)
+    from := msg.Info.Chat
+    sender := msg.Info.Sender.String()
+    pushName := msg.Info.PushName
+    isOwner := strings.Contains(sender, owner)
+    //isAdmin := m.GetGroupAdmin(from, sender)
+    isBotAdm := m.GetGroupAdmin(from, botNumber + "@s.whatsapp.net")
+    isGroup := msg.Info.IsGroup
+    args := strings.Split(m.GetCMD(), " ")
+    command := strings.ToLower(args[0])
+    query := strings.Join(args[1: ], ` `)
+    //extended := msg.Message.GetExtendedTextMessage()
+    //quotedMsg := extended.GetContextInfo().GetQuotedMessage()
+    //quotedImage := quotedMsg.GetImageMessage()
+    //quotedVideo := quotedMsg.GetVideoMessage()
+    //quotedSticker := quotedMsg.GetStickerMessage()
+    
+	// Self
+    if self && !isOwner {
+        return
+    }
+
+    //-- CONSOLE LOG
+    fmt.Println("\n===============================\nNAME: " + pushName + "\nJID: " + sender + "\nTYPE: " + msg.Info.Type + "\nMessage: " + m.GetCMD() + "")
+    fmt.Println(m.Msg.Message.GetPollUpdateMessage().GetMetadata())
+    switch command {
+
+        /* panggil bot */
+        case "bot":
+            m.Reply(`Bot aktif *` + pushName + `*`)
+            m.React("🤖")
+
+       
+		// end
+		}
+}
